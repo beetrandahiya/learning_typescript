526610004ba83424d8103560016b992459a236f2
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-26 12:46:13 +0530

    initial

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hello World');
+function printinfo(name, age, designation) {
+    return {
+        name: name,
+        age: age,
+        designation: designation
+    };
+}
+console.log(printinfo('sachin', 24, 'developer'));
---
+++ main.ts
@@ -0,0 +1,14 @@
+console.log('Hello World');
+
+function printinfo (name:string,age:number,designation:string):object{
+
+    return {
+        name:name,
+        age:age,
+        designation:designation
+    }
+    
+}
+
+console.log(printinfo('sachin',24,'developer'));
+
