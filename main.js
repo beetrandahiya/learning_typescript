4ccb9ff617ef3ed45c871a3859ea444880b48d25
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-03-27 13:25:59 +0530

    concerns

--- main.js
+++ main.js
@@ -21,3 +21,9 @@ function add_employee(info) {
     };
 }
 add_employee({ name: 'sachin', age: 24, designation: 'developer' });
+/*Trying to write least HTML and even write the page content in TS*/
+var body_content = document.createElement('div');
+body_content.innerHTML = '';
+body_content.innerHTML += '<div class="what"><h1>But, What is TypeScript?</h1><h3>TypeScript is just Javascript more like Java.</h3><p>TypeScript is a superset of Javascript. In simpler words, it is Javascript with some extra features.</p></div>';
+body_content.innerHTML += '<div class="concerns"><h1>While Learning a Language, our first concern is:</h1><h2>What is the purpose of the language?</h2><h2>What are the pros and cons of the Language?</h2>';
+document.body.appendChild(body_content);
